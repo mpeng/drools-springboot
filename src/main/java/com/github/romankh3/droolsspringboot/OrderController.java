4631ce32cf01286053b2c9ec42ae162f314592f5
wensheng.peng <wensheng.peng@cotiviti.com>
2024-02-11 10:49:23 -0500

    Add more endpoint and add howto.txt

--- src/main/java/com/github/romankh3/droolsspringboot/OrderController.java
+++ src/main/java/com/github/romankh3/droolsspringboot/OrderController.java
@@ -1,7 +1,8 @@
 package com.github.romankh3.droolsspringboot;
 
 import org.kie.api.runtime.KieSession;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.kie.api.runtime.rule.FactHandle;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final KieSession kieSession;
+    private FactHandle factHandle;
 
     public OrderController(KieSession kieSession) {
         this.kieSession = kieSession;
     }
 
     @PostMapping("/order")
-    public Order processRules(@RequestBody Order order) {
-        kieSession.insert(order);
+    public Order processRulesForInsert(@RequestBody Order order) {
+        factHandle = kieSession.insert(order);
         kieSession.fireAllRules();
         return order;
     }
+
+    @PutMapping("/order")
+    public Order processRulesForUpdate(@RequestBody Order order) {
+        if (null != factHandle) {
+            kieSession.update(factHandle, order);
+            kieSession.fireAllRules();
+        } else {
+            factHandle = kieSession.insert(order);
+            kieSession.fireAllRules();
+        }
+        return order;
+    }
+
+    @GetMapping("/order")
+    public Order processRulesForGet() {
+        System.out.println("processRulesForGet: " + factHandle );
+        Order order = null;
+        if (null != factHandle) {
+            order = (Order)kieSession.getObject(factHandle);
+            kieSession.fireAllRules();
+        }
+        return order;
+    }
+
 }
